309251cf42308d3e4820b76a82ee27520cc82f4d
ZsgsDesign <zsgsdesign@gmail.com>
2017-08-22 11:12:32 +0800

    remove unnecessary test files

--- protected/config.php
+++ protected/config.php
@@ -16,19 +16,6 @@ $config = array(
 );
 
 $domain = array(
-	"test.1cf.co" => array( // 调试配置
-		'debug' => 1,
-		'mysql' => array(
-
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => 'hola',
-				'MYSQL_PASS' => 'lalala',
-				'MYSQL_CHARSET' => 'utf8',
-
-		),
-	),
 	"hola.party" => array( // 调试配置
 		'debug' => 1,
 		'mysql' => array(
--- protected/controller/JumpController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class JumpController extends BaseController {
-	// 首页
-	function actionIndex(){
-		header("Location: https://www.ihola.one"); 
-	}
-}
