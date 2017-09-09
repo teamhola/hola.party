d75df1f07aa3bd865fc774d43b51b89b7597d666
ZsgsDesign <zsgsdesign@gmail.com>
2017-09-10 01:19:34 +0800

    fixed secure problems

--- protected/config.php
+++ protected/config.php
@@ -18,19 +18,6 @@ $config = array(
 );
 
 $domain = array(
-    "test.1cf.co" => array( // 调试配置
-		'debug' => 1,
-		'mysql' => array(
-
-				'MYSQL_HOST' => 'localhost',
-				'MYSQL_PORT' => '3306',
-				'MYSQL_USER' => 'root',
-				'MYSQL_DB'   => 'hola',
-				'MYSQL_PASS' => 'Zsgscorp1',
-				'MYSQL_CHARSET' => 'utf8',
-
-		),
-	),
 	"hola.party" => array( // 调试配置
 		'debug' => 1,
 		'mysql' => array(
